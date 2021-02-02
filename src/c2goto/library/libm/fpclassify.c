e4fcfdebdc23b8c1977bc458e088f8c9ff2d467c
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2021-02-02 20:47:50 -0300

    Revert "Added __builtin_signbit"
    This reverts commit a036883330697e81b0ff22815c8be0b93aff466d.

--- src/c2goto/library/libm/fpclassify.c
+++ src/c2goto/library/libm/fpclassify.c
@@ -3,7 +3,6 @@
 #ifdef _MSVC
 #define _USE_MATH_DEFINES
 #define _CRT_FUNCTIONS_REQUIRED 0
-
 #endif
 #include <math.h>
 #ifdef _MSVC
@@ -30,7 +29,6 @@ _signbit(float, _fdsign);
 #ifdef _MSVC
 #undef isnan
 #undef isinf
-
 #define classify_return_type short
 
 #define _signbit(type, name)                      \
