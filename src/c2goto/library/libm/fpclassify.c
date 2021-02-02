9bef3b84dbf5fa8d0610e90f41443ff82233c083
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2021-02-02 20:47:50 -0300

    Revert "fixed code-style"
    This reverts commit e3af0b8cb4a448f2cd25be6385828679189a8899.

--- src/c2goto/library/libm/fpclassify.c
+++ src/c2goto/library/libm/fpclassify.c
@@ -12,13 +12,12 @@
 
 #define classify_return_type short
 
-#define _signbit(type, name)                                                   \
-  int name(type d)                                                             \
-  {                                                                            \
-  __ESBMC_HIDE:;                                                               \
-    \ 
-        return __builtin_signbit((float)d);                                    \
-  }
+#define _signbit(type, name)                      \
+    int name(type d)                              \
+    {                                             \
+        __ESBMC_HIDE:;                            \ 
+        return __builtin_signbit((float)d); \
+    }
 
 _signbit(double, _dsign);
 _signbit(long double, _ldsign);
