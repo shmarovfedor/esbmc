8405e5ae59e6c776514754f5bcb9a9a2e476c64d
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2021-02-02 20:47:50 -0300

    Revert "Added OSConfiguration modules"
    This reverts commit 9ee8bad4b4668ee76cd8bd8685dff1a5bebfcc5f.

--- scripts/cmake/AppleConfiguration.cmake
+++ scripts/cmake/AppleConfiguration.cmake
@@ -6,15 +6,13 @@ if (APPLE)
 	set(CMAKE_C_ARCHIVE_CREATE   "<CMAKE_AR> Scr <TARGET> <LINK_FLAGS> <OBJECTS>")
 	set(CMAKE_CXX_ARCHIVE_CREATE "<CMAKE_AR> Scr <TARGET> <LINK_FLAGS> <OBJECTS>")
 	set(CMAKE_C_ARCHIVE_FINISH   "<CMAKE_RANLIB> -no_warning_for_no_symbols -c <TARGET>")
-    set(CMAKE_CXX_ARCHIVE_FINISH "<CMAKE_RANLIB> -no_warning_for_no_symbols -c <TARGET>")   
-        
-    # Note: This should check for the CMake version instead of OS
-    cmake_policy(SET CMP0079 NEW)
+    set(CMAKE_CXX_ARCHIVE_FINISH "<CMAKE_RANLIB> -no_warning_for_no_symbols -c <TARGET>")
 
     # MacOS Z3 does not need libgomp
     set(LIBGOMP_LIB "")
-    set(OS_FLEX_SMTLIB_FLAGS "")
-    set(OS_X86_INCLUDE_FOLDER "/usr/include/${CMAKE_LIBRARY_ARCHITECTURE}")
-    set(OS_C2GOTO_FLAGS "")
-    set(OS_Z3_LIBS "stdc++ -pthread")
+
+    # Set policies
+    cmake_policy(SET CMP0079 NEW)
+else()
+    set(LIBGOMP_LIB "-lgomp -ldl")
 endif()
